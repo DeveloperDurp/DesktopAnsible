9c2035e09c71395ea6c492db86305c9b85b3a2a8
user <user@pop-os.localdomain>
2023-07-15 19:39:13 -0500

    update

--- scripts/configure-ssh.ps1
+++ scripts/configure-ssh.ps1
@@ -1,31 +1,35 @@
+param ($GITLAB_TOKEN)
+if (!$GITLAB_TOKEN){
+  Write-Output "Please unlock Bitwarden"
+  break
+}
+
+
+
 if(!(test-path ~/.ssh/id_rsa)){
-    ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N '""'
+    ssh-keygen -a 100 -t ed25519 -f ~/.ssh/id_rsa -q -N '""'
 }
 
 #Gitlab set ssh key
 
-if($(Test-Path "~/.credentials/gitlab" )){
-    TOKEN =  Get-Content "~/.credentials/gitlab" | ConvertTo-SecureString-AsPlainText
+$header = @{
+    "PRIVATE-TOKEN"=$GITLAB_TOKEN
+}
+$GitlabAPI = "https://gitlab.com/api/v4"
 
-    $header = @{
-        "PRIVATE-TOKEN"=$TOKEN
-    }
-    $GitlabAPI = "https://gitlab.com/api/v4"
-    
-    $publickey = Get-Content ~/.ssh/id_rsa.pub
-    
-    $body = @{
-        title = "Ansible Script"
-        key = "$publickey"
-        expires_at ="$((get-date).AddDays(30).ToUniversalTime().ToString("o"))"
-    } | ConvertTo-Json
-    
-    Try{
-        Invoke-RestMethod -Headers $header -Uri $GitlabAPI/user/keys -Body $body -Method Post -ContentType application/json -ErrorVariable gitlabkey | Out-Null
-    }Catch{
-        if($gitlabkey -notlike "*has already been taken*"){
-            Write-Error "Failed to upload key"
-            exit -1
-        }
+$publickey = Get-Content ~/.ssh/id_rsa.pub
+
+$body = @{
+    title = "Ansible Script"
+    key = "$publickey"
+    expires_at ="$((get-date).AddDays(30).ToUniversalTime().ToString("o"))"
+} | ConvertTo-Json
+
+Try{
+    Invoke-RestMethod -Headers $header -Uri $GitlabAPI/user/keys -Body $body -Method Post -ContentType application/json -ErrorVariable gitlabkey | Out-Null
+}Catch{
+    if($gitlabkey -notlike "*has already been taken*"){
+        Write-Error "Failed to upload key"
+        exit -1
     }
 }
--- scripts/credential-file.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    $service,
-    $password
-) 
-
-if($service -eq "gitlab"){
-    $password | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Out-File ~/.credentials/gitlab
-}
